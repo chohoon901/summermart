6bfee1d1e9fd4ba9027f647133663fb4e1cb096c
JoySoon <swk4838@gmail.com>
2023-08-10 17:33:18 +0900

    Repository 수정 및 테스트, Controller 임시 무력화, 설정 추가

--- src/main/java/com/example/demo/prod/ProdRepository.java
+++ src/main/java/com/example/demo/prod/ProdRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.prod;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Repository
 public interface ProdRepository extends JpaRepository<Prod, String> {
 
-    @Query(value = "SELECT PROD FROM prod_tbl2 WHERE PROD LIKE  %:word%", nativeQuery = true)
-    public List<Prod> findProd();
+    @Query(value = "SELECT * FROM prod_tbl2 WHERE PROD LIKE %:word% LIMIT 5", nativeQuery = true)
+    List<Prod> findProd(@Param(value="word") String word);
+
 }
--- src/test/java/com/example/demo/SummermartApplicationTests.java
+++ src/test/java/com/example/demo/SummermartApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.prod.Prod;
+import com.example.demo.prod.ProdRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SummermartApplicationTests {
+	@Autowired
+	ProdRepository repository;
 
 	@Test
 	void contextLoads() {
+		List<Prod> result = repository.findProd("메밀");
+		System.out.println(result);
 	}
 
 }
