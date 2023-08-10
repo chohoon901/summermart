8597b418ea6e3c801ba73e398d3d62f453ec1ec4
JoySoon <swk4838@gmail.com>
2023-08-10 17:54:18 +0900

    Controller 작성

--- src/main/java/com/example/demo/prod/ProdController.java
+++ src/main/java/com/example/demo/prod/ProdController.java
@@ -1,46 +1,53 @@
-//package com.example.demo.prod;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.client.RestTemplate;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@RestController
-//public class ProdController {
-//
-//    private final String FLASK_SERVER_URL = "http://127.0.0.1:5000"; // Flask 서버 주소
-//
-//    @Autowired
-//    private ProdRepository prodRepository;
-//
-//    @PostMapping("/search")
-//    public ResponseEntity<List<String>> getSimilarProducts(@RequestBody String inputWord) {
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        // Flask 서버의 API 엔드포인트에 요청을 보냄
-//        ResponseEntity<List> response = restTemplate.postForEntity(
-//                FLASK_SERVER_URL + "/get_similar_products",
-//                "{\"input_word\": \"" + inputWord + "\"}",
-//                List.class
-//        );
-//
-//        List<String> similarProductNamesFromFlask = response.getBody();
-//
-//        // Spring Data JPA를 이용하여 데이터베이스에서 유사한 제품 정보를 찾음
-////        List<Prod> similarProductsFromDB = prodRepository.findProd("%" + inputWord + "%");
-//
-//        // 결과를 합침 (Flask 결과 + 데이터베이스 결과)
-////        similarProductNamesFromFlask.addAll(
-////                similarProductsFromDB.stream()
-////                        .map(Prod::getPROD)
-////                        .collect(Collectors.toList())
-////        );
-//
-//        return ResponseEntity.ok(similarProductNamesFromFlask);
-//    }
-//}
+package com.example.demo.prod;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+public class ProdController {
+
+    @Autowired
+    private ProdRepository prodRepository;
+
+    @RequestMapping("/search")
+
+    @GetMapping("/get_similar_products")
+    public ResponseEntity<List<Prod>> getSimilarProducts(@RequestParam String input_word) {
+        List<Prod> similarProducts = findSimilarProducts(input_word);
+        return ResponseEntity.ok(similarProducts);
+    }
+
+    private List<Prod> findSimilarProducts(String inputWord) {
+        List<Prod> similarProducts = new ArrayList<>();
+
+        // Find similar words using the loaded model
+        List<String> similarWords = findSimilarWords(inputWord);
+
+        for (String word : similarWords) {
+            List<Prod> prodList = prodRepository.findProd("%" + word + "%");
+            similarProducts.addAll(prodList);
+        }
+
+        // Randomly limit results to 5
+        Collections.shuffle(similarProducts);
+        int limit = Math.min(similarProducts.size(), 5);
+        return similarProducts.subList(0, limit);
+    }
+
+    private List<String> findSimilarWords(String inputWord) {
+        // Use loaded_model.wv.most_similar to find similar words using the loaded model
+        // Implement this part based on your loaded model
+        // For now, let's assume similar words are hardcoded
+        List<String> similarWords = Arrays.asList();
+        return similarWords;
+    }
+}
