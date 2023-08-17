8820a3f1063e332a4ae5bfde1c86167f6c59a830
JoySoon <swk4838@gmail.com>
2023-08-17 19:56:15 +0900

    ProductList 제작 (임시)

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/member")
+//@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
--- src/main/java/com/example/demo/controller/ProductListController.java
+++ src/main/java/com/example/demo/controller/ProductListController.java
@@ -1,9 +1,44 @@
 package com.example.demo.controller;
 
-import lombok.RequiredArgsConstructor;
+import com.example.demo.dto.ProductListDTO;
+import com.example.demo.service.ProductListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/product-list")
 public class ProductListController {
+
+    private final ProductListService productListService;
+
+    @Autowired
+    public ProductListController(ProductListService productListService) {
+        this.productListService = productListService;
+    }
+
+    @GetMapping
+    public List<ProductListDTO> getProductList() {
+        return productListService.getProductList();
+
+        //    @GetMapping("/display")
+//    public List<MainDisplayDTO> getTopProducts() {
+//        int numberOfProductsToDisplay = 10; // 표시할 상품 수
+//        return mainDisplayService.getTopProducts(numberOfProductsToDisplay);
+//    }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
