6295c63d80aff7fb4c65d704ae4e49440a8d34cb
JoySoon <swk4838@gmail.com>
2023-08-17 12:49:09 +0900

    mainpage 배너, 상품 이미지 보여주기

---
+++ src/main/java/com/example/demo/dto/MainPageDTO.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class MainPageDTO {
+    private Long id;
+    private String name;
+    private int price;
+    private String picture;
+
+}
